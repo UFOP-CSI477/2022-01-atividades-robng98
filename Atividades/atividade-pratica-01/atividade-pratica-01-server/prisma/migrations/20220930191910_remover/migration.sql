701d7b4057b0605b66d04a6e96d6ccc7216a013f
robng98 <betonicacio10@gmail.com>
2022-10-01 01:21:47 -0300

    Cidade_id removida da tabela pessoas

---
+++ Atividades/atividade-pratica-01/atividade-pratica-01-server/prisma/migrations/20220930191910_remover/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `cidade_id` on the `pessoas` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "pessoas" DROP COLUMN "cidade_id";
